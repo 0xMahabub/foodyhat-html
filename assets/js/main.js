6a129982e4ac4ae5af3c4021f368aea0549319a0
Md Mahabub Alam <mahabub212002072@gmail.com>
2021-10-03 14:25:48 +0600

    menu staging

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,26 @@
     "use strict";
 
     // script starts here
+    var navMenu = document.getElementById('navmenu');
+    var catbtn = document.getElementById('catmenubtn');
+    var catmenu = document.getElementById('catmenulist');
+
+
+
+
+    //navmenu 
+    navMenu.querySelectorAll('li').forEach(itm => {
+        itm.addEventListener('click', () => {
+            if(itm.classList.contains('relative')) {
+                itm.querySelector('ul').classList.toggle('hidden');
+            }
+        })
+    });
+
+    // catmenu
+    catbtn.addEventListener('click', () => {
+        catmenu.classList.toggle('hidden');
+    });
 
     //
 })();
